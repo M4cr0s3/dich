b02523d188224e3a4e23dc75dd905bf1e450cf13
Daniil <verapushtrinova@gmail.com>
2024-10-04 10:50:30 +0300

    ЗДЕСЬ ЧЁРНЫМ ПО БЕЛОМУ НАПИСАНО ГОООООЙДАААА

--- resources/views/components/entrance-card.blade.php
+++ resources/views/components/entrance-card.blade.php
@@ -8,5 +8,5 @@
     <img src="{{ asset($image) }}" alt="{{ $title }}">
     <span class="text-center text-xl mt-5 break-words font-thin">{{ $title }}</span>
     <span class="font-bold mt-2 mb-4 text-2xl">{{ $price }} ₽</span>
-    <a class="px-9 py-2.5 bg-[#C2C2C2] rounded-2xl font-bold text-white cursor-pointer">В корзину</a>
+    <a class="px-9 py-2.5 bg-[#C2C2C2] rounded-full font-bold text-white cursor-pointer">В корзину</a>
 </div>
